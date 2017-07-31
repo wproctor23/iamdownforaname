01283d30c2ebd3d31ca1e8e94d255219f1b7a479
wproctor23 <wproctor23@yahoo.com>
2017-07-31 16:23:45 -0700

    initalize git

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+mixins = require('postcss-mixins'),
+cssImport = require('postcss-import');
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+      .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+      .on('error', function(error) {
+        console.log(error.toString());
+        this.emit('end');
+      })
+      .pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+browserSync = require('browser-sync').create();
+watch = require('gulp-watch');
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream())
+});
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
+});
